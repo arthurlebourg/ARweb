b1761633252e4b4c7b3e56e7e6a9dc023aaae4c9
Arthur Le Bourg <lebourgarthur@gmail.com>
2023-01-24 00:40:48 +0100

    added beningininging of communcation data channel peer to peer

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ var remoteVideo: any;
 var peerConnection: any;
 export var uuid: string;
 export var serverConnection: any;
-var localVideo : any;
-
+var localVideo: any;
+export var dataChannel: any;
 
 var peerConnectionConfig = {
   'iceServers': [
-    {'urls': 'stun:stun.stunprotocol.org:3478'},
-    {'urls': 'stun:stun.l.google.com:19302'},
+    { 'urls': 'stun:stun.stunprotocol.org:3478' },
+    { 'urls': 'stun:stun.l.google.com:19302' },
   ]
 };
 
@@ -26,72 +26,83 @@ function createUUID() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
-function getUserMediaSuccess(stream : any) {
+function getUserMediaSuccess(stream: any) {
   localStream = stream;
   localVideo.srcObject = stream;
 }
 
-function start(isCaller : boolean) {
+function start(isCaller: boolean) {
   console.log('isCaller : ' + isCaller)
   peerConnection = new RTCPeerConnection(peerConnectionConfig);
   peerConnection.onicecandidate = gotIceCandidate;
   peerConnection.ontrack = gotRemoteStream;
   peerConnection.addStream(localStream);
+  dataChannel = peerConnection.createDataChannel("chat", { negotiated: true, id: 0 });
+  dataChannel.onopen = (event : any) => {
+    dataChannel.send('Hi!');
+  }
+  dataChannel.onmessage = (event : any) => {
+    console.log('penis: ' + event.data);
+  }
 
-  if(isCaller) {
-    peerConnection.addStream(localStream);
+  peerConnection.ondatachannel = (event : any) => {
+    const receiveChannel = event.channel;
+    receiveChannel.onmessage = (event : any) => {
+      console.log('bite: ' + event.data);
+    }
+  }
+
+  if (isCaller) {
     peerConnection.createOffer().then(createdDescription).catch(errorHandler);
   }
 }
 
-function gotMessageFromServer(message : any) {
-  if(!peerConnection) start(false);
+function gotMessageFromServer(message: any) {
+  if (!peerConnection) start(false);
 
   let signal = JSON.parse(message.data);
 
 
   // Ignore messages from ourself
-  if(signal.uuid == uuid) return;
+  if (signal.uuid == uuid) return;
 
-  if(signal.sdp)
-  {
+  if (signal.sdp) {
     peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function () {
       // Only create answers in response to offers
-      if (signal.sdp.type == 'offer')
-      {
+      if (signal.sdp.type == 'offer') {
         peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
       }
     }).catch(errorHandler);
 
+
   }
-  else if (signal.ice)
-  {
+  else if (signal.ice) {
     peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
   }
 }
 
-function gotIceCandidate(event : any) {
-  if(event.candidate != null) {
-    var msg = JSON.stringify({'ice': event.candidate, 'uuid': uuid});
+function gotIceCandidate(event: any) {
+  if (event.candidate != null) {
+    var msg = JSON.stringify({ 'ice': event.candidate, 'uuid': uuid });
     serverConnection.send(msg);
   }
 }
 
-function createdDescription(description : any) {
+function createdDescription(description: any) {
   console.log('got description');
 
-  peerConnection.setLocalDescription(description).then(function() {
-    var msg = JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid});
+  peerConnection.setLocalDescription(description).then(function () {
+    var msg = JSON.stringify({ 'sdp': peerConnection.localDescription, 'uuid': uuid });
     serverConnection.send(msg);
   }).catch(errorHandler);
 }
 
-function gotRemoteStream(event : any) {
+function gotRemoteStream(event: any) {
   console.log('got remote stream');
   remoteVideo.srcObject = event.streams[0];
 }
 
-function errorHandler(error : any) {
+function errorHandler(error: any) {
   console.log(error);
 }
 
@@ -104,13 +115,7 @@ document.addEventListener("DOMContentLoaded", () => {
   localVideo = document.getElementById('localVideo');
   remoteVideo = document.getElementById('remoteVideo');
 
-  if (navigator.xr)
-  {
-    const displayMediaOptions = {
-      video: true,
-      audio: false
-    };
-
+  if (navigator.xr) {
     let button_AR = document.createElement("input");
     button_AR.type = "button";
     button_AR.value = "Start AR";
@@ -120,11 +125,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
       start(true);
     };
-    
+
     document.body.appendChild(button_AR);
   }
-  if (navigator.mediaDevices.getUserMedia)
-  {
+  if (navigator.mediaDevices.getUserMedia) {
     let constraints = {
       video: true,//{ width: 1280, height: 720 }, { frameRate: { ideal: 10, max: 15 } }, { facingMode: (front? "user" : "environment") } 
       audio: false,
@@ -137,45 +141,20 @@ document.addEventListener("DOMContentLoaded", () => {
       let stream = await navigator.mediaDevices.getUserMedia(constraints);
 
       getUserMediaSuccess(stream)
-      
+
       start(true);
     };
-
     document.body.appendChild(button);
+    
+    let button_click = document.createElement("input");
+    button_click.type = "button";
+    button_click.value = "Click";
+    button_click.onclick = function () {
+      dataChannel.send("click");
+    };
+    document.body.appendChild(button_click);
+
   } else {
     alert('Your browser does not support getUserMedia API nor WebXR');
   }
 });
-
-
-  
-/*  
-if (navigator.xr) 
-{
-  document.addEventListener("DOMContentLoaded", () => {
-    // get button element
-    const button = document.getElementById("xr")!;
-    button.addEventListener("click", activateAR);
-  });
-}
-else {
-  //get image element with id 'video'
-  var video_elm = document.getElementById("video") as HTMLImageElement;
-  socket.on('computer video', data => {
-    console.log("recived video");
-    video_elm.src = data;
-    //saveFile(data.replace("image/jpeg", "image/octet-stream"), "test.jpg");
-  });
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const button = document.getElementById("xr")!;
-    button.parentNode!.removeChild(button);
-
-    // create text element
-    var text = document.createElement("p");
-    text.innerHTML = "Your browser does not support WebXR";
-    document.body.appendChild(text);
-    
-  });
-  
-};*/
