3b1c575945d9da574026a1caa2aa448e4628a475
Arthur Le Bourg <lebourgarthur@gmail.com>
2023-01-26 21:49:08 +0100

    changed port

--- run_server_doker.sh
+++ run_server_doker.sh
@@ -4,4 +4,4 @@ npm run build
 
 docker build -t archat:latest .
 
-docker run -d -p 3001:3001 --restart unless-stopped archat:latest
+docker run -d -p 3000:3000 --restart unless-stopped archat:latest
--- server.js
+++ server.js
@@ -41,6 +41,6 @@ wss.broadcast = function (data) {
     });
 };
 
-server.listen(process.env.PORT || 3001, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log(`Server running at http://127.0.0.1:${server.address().port}/`);
 });
