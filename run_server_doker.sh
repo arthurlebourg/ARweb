0b4c06cea2e178aae105641898e4727381b943bb
Arthur Le Bourg <lebourgarthur@gmail.com>
2023-01-27 16:01:27 +0100

    fix mounting files

--- run_server_doker.sh
+++ run_server_doker.sh
@@ -4,4 +4,4 @@ npm run build
 
 docker build -t archat:latest .
 
-docker run -d -p 443:3000 -v/certifications:/etc/letsencrypt/live/ar.arthurlb.fr/ --restart unless-stopped archat:latest
+docker run -d -p 443:3000 -v /etc/letsencrypt/live/ar.arthurlb.fr/:/usr/src/app/certifications --restart unless-stopped archat:latest
